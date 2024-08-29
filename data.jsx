9b9038749b63e581dc11108a0452862fde6c509a
LucyC-1225 <lucyc1225xx@gmail.com>
2024-08-29 10:30:32 -0400

    updated data

---
+++ data.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+
+export const drumPadData = [{
+    keyLetter: "Q",
+    audioSrc: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3",
+    displayText: "Heater 1"
+  },{
+    keyLetter: "W",
+    audioSrc: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3",
+    displayText: "Heater 2"
+  },{
+    keyLetter: "E",
+    audioSrc: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3",
+    displayText: "Heater 3"
+  },{
+    keyLetter: "A",
+    audioSrc: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3",
+    displayText: "Heater 4"
+  },{
+    keyLetter: "S",
+    audioSrc: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3",
+    displayText: "Clap"
+  },{
+    keyLetter: "D",
+    audioSrc: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3",
+    displayText: "Open-HH"
+  },{
+    keyLetter: "Z",
+    audioSrc: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3",
+    displayText: "Kick-n'-Hat"
+  },{
+    keyLetter: "X",
+    audioSrc: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3",
+    displayText: "Kick"
+  },{
+    keyLetter: "C",
+    audioSrc: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3",
+    displayText: "Closed-HH"
+  }];
